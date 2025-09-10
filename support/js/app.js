21fa4fec00f6984235059a041726fdc1b3d1f7ec
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-10 12:52:32 +0330

    Storage #1

--- support/js/app.js
+++ support/js/app.js
@@ -1,1 +1,9 @@
-git 
+const h1 = document.getElementById("title");
+const firstName = localStorage.getItem("firstName");
+
+localStorage.setItem("firstName", "Mostafa");
+localStorage.setItem("lasName", "Mohammadimehr");
+// localStorage.removeItem("firstName");
+firstName
+  ? (h1.innerText = `welcom  : ${firstName} `)
+  : (h1.innerText = `no body to welcom`);
