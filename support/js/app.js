fc050fe3ef5869568fe5e5a3498ddce3f589e12f
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-10 14:20:05 +0330

    Storage Practice #1

--- support/js/app.js
+++ support/js/app.js
@@ -1,10 +1,15 @@
-const h1 = document.getElementById("title");
-const firstName = localStorage.getItem("firstName");
+const title = document.getElementById("title");
+const mybtn = document.getElementById("mybtn");
+let input = document.getElementById("input");
 
-localStorage.setItem("firstName", "Mostafa");
-localStorage.setItem("lasName", "Mohammadimehr");
-localStorage.removeItem("firstName");
-firstName
-  ? (h1.innerText = `welcom  : ${firstName} `)
-  : (h1.innerText = `no body to welcom`);
-  
+mybtn.addEventListener("click", () => {
+  let inputvalue = input.value.trim();
+  localStorage.setItem("firstname", inputvalue);
+  let firstname = localStorage.getItem("firstname");
+
+  if (inputvalue) {
+    title.innerText = `خوش آمدید آقا ${firstname}`;
+  } else {
+    title.innerText = `نام شما یافت نشد دوباره تلاش کنید.`;
+  }
+});
